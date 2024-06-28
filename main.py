7ccf95cfdc7066b8d24ad439aac24f97ed15c177
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-27 21:46:31 -0700

    Completed

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 def main():
-    
+    flag=1
     numbers = []
-    previous_input = 0
-    current_input = 0
-    while (current_input < previous_input):
-        current_input = int(input('Enter a number'))
-        numbers.append(current_input)
-        print(numbers)
-        if (current_input > previous_input):
-            print(numbers, end='')
+    previous_input = int(100)
+    current_input = int(input('Enter a number: '))
+    
+    while (flag):
+        
+        if ( current_input > previous_input):
+            flag=0
+            print(numbers, end=' ')
             break
         else:
+            numbers.append(current_input)
             previous_input = current_input
+            current_input = int(input('Enter a number: '))
+            continue
+            
+            
 
 
-            return numbers
+            
         
         
 
